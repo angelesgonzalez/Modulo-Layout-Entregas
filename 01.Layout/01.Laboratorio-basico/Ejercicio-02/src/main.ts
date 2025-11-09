a6939961aa1ddfef4b881f0c83f0331861990f8e
Angeles <delosangelesgonzalez@gmail.com>
2025-11-09 14:41:30 +0100

    Add a simple logic to change between light/dark theme

--- 01.Layout/01.Laboratorio-basico/Ejercicio-02/src/main.ts
+++ 01.Layout/01.Laboratorio-basico/Ejercicio-02/src/main.ts
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const themeToggleButton = document.getElementById("theme-toggle");
+themeToggleButton?.addEventListener("click", () => {
+	if (document.documentElement.getAttribute("data-theme") === "dark") {
+		document.documentElement.removeAttribute("data-theme");
+	} else {
+		document.documentElement.setAttribute("data-theme", "dark");
+	}
+});
